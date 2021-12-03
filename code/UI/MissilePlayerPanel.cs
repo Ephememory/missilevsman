bf08f2d94023e61fef659b1b1960a4c968eeafcd
Ephememory <leetvipertm@gmail.com>
2021-12-03 16:48:59 -0600

    Add missile tutorial ui box on respawn

--- code/UI/MissilePlayerPanel.cs
+++ code/UI/MissilePlayerPanel.cs
@@ -15,10 +15,16 @@ namespace Missile.UI
 	{
 		public MissilePlayer missilePlayerPawn { get; private set; } //We can only hope to not lose this cached reference..
 		private Panel launchingStatus;
+		private Panel tutorial;
+		private float tutorialFadeout = 1;
 		public MissilePlayerPanel( MissilePlayer player )
 		{
 			missilePlayerPawn = player;
 			AddChild( new MissileLifeTimeBar( player ) );
+
+			tutorial = Add.Panel( "missile-tutorial" );
+			tutorial.Add.Label( "FORWARD or RUN to accelerate\nBACK or JUMP to decelerate\nMOUSE to steer" );
+
 			launchingStatus = Add.Panel( "launch-status-container" );
 			launchingStatus.Add.Label( "LAUNCHING MISSILE", "launch-status" );
 		}
@@ -26,6 +32,9 @@ namespace Missile.UI
 		public override void Tick()
 		{
 			launchingStatus.SetClass( "active", !(missilePlayerPawn.Controller as MissileController).SpawnGracePeriodFinished );
+			tutorialFadeout = MathX.Approach( tutorialFadeout, 0, Time.Delta / 6 );
+			tutorial.Style.Opacity = tutorialFadeout;
+			tutorial.Style.Dirty();
 			base.Tick();
 		}
 	}
